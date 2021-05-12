1c568954d9af867008a95620e747d5af8c7e9699
Ali Fallah <ali.fallah.rn@gmail.com>
2021-05-12 13:44:13 +0430

    Backend changes were done

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tasks;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Tasks::orderBy('created_at', 'DESC')->get();
+
+        return response($tasks, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $task = new Tasks();
+        $task->title = $request->task['title'];
+        $task->save();
+
+        return response($task, 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $task = Tasks::find($id);
+        if($task){
+            $task->completed = $request->task['completed'] ? true : false;
+            $task->completed_at = $request->task['completed'] ? Carbon::now() : null;
+            $task->save();
+            return response($task, 200);
+        }
+        return response('Task not found.', 404);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = Tasks::find($id);
+        if($task){
+            $task->delete();
+            return response('The task was successfully deleted.', 200);
+        }
+        return response('Task not found.', 404);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::prefix('task')->group(function () {
+    Route::post('/store', [TaskController::class, 'store']);
+    Route::put('/{id}', [TaskController::class, 'update']);
+    Route::delete('/{id}', [TaskController::class, 'destroy']);
 });
